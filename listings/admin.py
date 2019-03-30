5098f570b06ed9fbc14f211fb4a2b0d8709b0cdd
drubaby <peryton6@gmail.com>
2019-03-30 15:36:54 -0400

    customize admin display data

--- listings/admin.py
+++ listings/admin.py
@@ -3,5 +3,15 @@ from .models import Listing
 
 # Where we customize admin stuff for listings app
 
-# Register
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    # Single params need commas
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    # pagination
+    list_per_page = 25
+    
+# Register model
+admin.site.register(Listing, ListingAdmin)
