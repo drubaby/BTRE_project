5d3a3c377e79d63be1153bfc79e596dd5e3ee311
drubaby <peryton6@gmail.com>
2019-03-30 17:29:08 -0400

    add pagination, order get all by published and date

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
 def index(request):
-    # Fetch all from db
-    listings = Listing.objects.all()
+    # Fetch all from db ordered by descending list date, filter by published
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
+
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
