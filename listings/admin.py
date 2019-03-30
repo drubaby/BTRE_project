b8e96bf4c037dd31b04a08c2683264b154cd81b6
drubaby <peryton6@gmail.com>
2019-03-30 14:17:52 -0400

    create superuser, register models with admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+# Where we customize admin stuff for listings app
+
+# Register
+admin.site.register(Listing)
