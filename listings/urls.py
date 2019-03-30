db358b010015232de3c8e0c7c9ac8ae907ef56ad
drubaby <peryton6@gmail.com>
2019-03-30 16:59:59 -0400

    display /listings in template

--- listings/urls.py
+++ listings/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='listings'),
 
     # parameter of listing id integer
-    path('<int: listing_id>', views.listing, name='listing '),
+    path('<int:listing_id>', views.listing, name='listing'),
 
     path('search', views.search, name='search')
 ]
