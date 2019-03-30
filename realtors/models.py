8f2d792d30167b186d6e8dddb1ea944010d0bd9e
drubaby <peryton6@gmail.com>
2019-03-30 14:04:59 -0400

    make migrations

--- realtors/models.py
+++ realtors/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from datetime import datetime
 # Create your models here.
-class Realtor(models.model):
+class Realtor(models.Model):
     name = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     # desc is optional
@@ -12,4 +12,3 @@ class Realtor(models.model):
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.name
-    
